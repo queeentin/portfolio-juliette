11ea07424d26933c1ef7ade9f9c4354f03ed8a4b
queeentin <hello@quentinastie.space>
2024-02-08 11:25:05 +0100

    Premier commit

---
+++ content/pages/apropos.md
@@ -0,0 +1,4 @@
+Title: À propos
+Date: 2024-01-01
+Status: published
+Template: apropos
---
+++ pelicanconf.py
@@ -0,0 +1,49 @@
+AUTHOR = "Quentin"
+SITENAME = "Juliette Nier"
+SITEURL = ""
+
+PATH = "content"
+STATIC_PATHS = ["projets"]
+
+THEME = "themes/juliette"
+
+TIMEZONE = "Europe/Zurich"
+
+DEFAULT_LANG = "fr"
+
+# Put pages at the root of the website
+PAGE_URL = "{slug}.html"
+PAGE_SAVE_AS = "{slug}.html"
+
+# Don"t generate useless pages on authors, categories, etc.
+DIRECT_TEMPLATES = ["index"]
+AUTHOR_SAVE_AS = ""
+ARCHIVES_SAVE_AS = ""
+ARTICLE_SAVE_AS = ""
+CATEGORY_SAVE_AS = ""
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (
+    ("Pelican", "https://getpelican.com/"),
+    ("Python.org", "https://www.python.org/"),
+    ("Jinja2", "https://palletsprojects.com/p/jinja/"),
+    ("You can modify those links in your config file", "#"),
+)
+
+# Social widget
+SOCIAL = (
+    ("You can add links in your config file", "#"),
+    ("Another social link", "#"),
+)
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+# RELATIVE_URLS = True
